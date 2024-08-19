639e5bbe22b5081dd24280f46dc1a76ad292c200
Surya <49099412+suryapandiaraj@users.noreply.github.com>
2024-08-19 20:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+student = {
+	"name" : "Surya Pandiaraj"
+}
+
+Object.prototype.getKeys = function (){
+	return Object.keys(this);
+}
+
+console.log(student.getKeys())
